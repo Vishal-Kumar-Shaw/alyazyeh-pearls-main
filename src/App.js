f324ec6ffd743c0ef12530092ac39d3ffd55eabe
Vishal-Kumar-Shaw <vkshaw1997@gmail.com>
2023-02-15 13:55:19 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Services from './components/Our Services/Services';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
-      Hello App
+      <Services/>
     </div>
   );
 }
---
+++ src/components/Our Services/Services.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Cards from './Cards'
+
+const Services = () => {
+  return (
+    <div>
+        <h1>Our Services </h1>
+        <Cards/>
+        <Cards/>
+        <Cards/>
+        <Cards/>
+        <Cards/>
+    </div>
+  )
+}
+
+export default Services
