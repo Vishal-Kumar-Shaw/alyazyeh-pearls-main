ac6db89bde31904af3ee2b31f3d1afd7ee298abd
PATILJAYESHSUKDEO <44015307+PATILJAYESHSUKDEO@users.noreply.github.com>
2023-02-22 14:23:13 +0530

    push changes

--- src/components/Enquire/Enquire.jsx
+++ src/components/Enquire/Enquire.jsx
@@ -18,11 +18,11 @@ export default function Enquire(){
     });
   }, []);
   return (
-      <MDBCard data-aos="fade-up" background='dark' className='text-white'>
+      <MDBCard data-aos="zoom-in" background='dark' className='text-white'>
        <div className='img' style={{height:"25vw", width:"100vw"}}><MDBCardImage style={{height:"100%", width:"100%"}} overlay src='./Images/d6.jpg' alt='...'/></div>
         <MDBCardOverlay>
           <div className='info'><MDBCardTitle><br></br>AVAIL EXPERT GEMMOLOGICAL SERVICES FROM THE MULTINATIONAL TEAM</MDBCardTitle>
-          <MDBCardText data-aos="fade-up">
+          <MDBCardText data-aos="zoom-out">
           <br></br>AlyazyehPearls takes pride in having a multinational and experienced team of gemologists, graders, analysts, polishers, and craftsmen that are instilled with a passion to perform, and continue to pursue excellence every day.
           </MDBCardText>
           </div>
