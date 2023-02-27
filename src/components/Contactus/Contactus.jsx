44d5e4a9696d9f7fe59105b1e80814e0d37f3016
PATILJAYESHSUKDEO <44015307+PATILJAYESHSUKDEO@users.noreply.github.com>
2023-02-27 11:37:36 +0530

    push final

--- src/components/Contactus/Contactus.jsx
+++ src/components/Contactus/Contactus.jsx
@@ -1,8 +1,70 @@
 import React from 'react';
-import { MDBInput, MDBCheckbox, MDBBtn, MDBTextArea } from 'mdb-react-ui-kit';
+import { MDBInput, MDBCheckbox, MDBBtn, MDBTextArea, MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBRow,
+  MDBCardImage,
+  MDBCol,
 
+   } from 'mdb-react-ui-kit';
+import './Contactus.css';
 export default function Contact() {
   return (
+    <>
+    <MDBCard>
+      <MDBCardBody>
+        <center>
+      <MDBCardImage className='logo' style={{height:"5%", width:"5%"}} src='./Images/logo.png' position='top' alt='...' />
+      </center>
+      <MDBCardTitle className='ct'> GET IN TOUCH WITH US</MDBCardTitle>
+        <MDBCardText>
+        For inquiries, or assistance, our representatives are available around-the-clock for you.
+        </MDBCardText>
+      </MDBCardBody>
+    </MDBCard>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+     <MDBRow>
+      <MDBCol sm='4'>
+        <MDBCard className='c3'>
+          <MDBCardBody>
+            <MDBCardTitle className='ct'>HEAD OFFICE</MDBCardTitle>
+            <MDBCardText>
+            Al Saqr Business Tower,DIFC/Sheikh Zayed Road,Dubai, 
+United Arab Emirates
+            </MDBCardText>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+      <MDBCol sm='4'>
+        <MDBCard className='c3'>
+          <MDBCardBody>
+            <MDBCardTitle className='ct'> EMAIL ID</MDBCardTitle>
+            <MDBCardText>
+            AlyazyehPearls@global.com
+            </MDBCardText>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+      <MDBCol sm='1'>
+        <MDBCard className='c3'>
+          <MDBCardBody>
+            <MDBCardTitle className='ct'>   TELEPHONE</MDBCardTitle>
+            <MDBCardText>
+            +971 4 358 9000
+            </MDBCardText>
+            </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+    </MDBRow>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <center>
     <form id='form' className='text-center' style={{ width: '100%', maxWidth: '300px' }}>
       <h2>Contact us</h2>
 
@@ -20,5 +82,9 @@ export default function Contact() {
         Send
       </MDBBtn>
     </form>
+    </center>
+    </>
   );
 }
+
+
