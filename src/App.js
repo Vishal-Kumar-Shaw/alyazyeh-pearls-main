4de261e549ede9abc87089d3439a68df36e21672
unknown <garimagarg1999@gmail.com>
2023-02-27 12:15:56 +0530

    Hello

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Services from './components/Our Services/Services';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header/Header'
 
 
 function App() {
   return (
     <div className="App">
-      <Services/>
+      <Header/>
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import Corousal from '../Corousal/Corousal'
+
+function Header(){
+    return (
+        <div>
+            <Corousal/>
+        </div>
+    )
+}
+
+export default Header;
